5233754fbe3797f89a61492b21184055e914b2b0
lolorahaingo <laurent@MacBook-Air-de-Laurent.local>
2018-07-29 18:59:43 +0200

    reset

--- system/read_stdin/ft_list_push_back.c
+++ system/read_stdin/ft_list_push_back.c
@@ -13,22 +13,12 @@
 #include "ft_list.h"
 #include <stdio.h>
 
-char *ft_data_cpy(void *data)
-{
-	char *str_return;
-
-	str_return = ((char *)data);
-	return (str_return);
-}
-
 void		ft_list_push_back(t_list **begin_list, void *data)
 {
 	t_list	*new_elem;
 	t_list *tmp;
 
-	//printf("%c\n", *((char *)data));
-	//printf("%p : %c  ", &a, a);
-	new_elem = ft_create_elem(ft_data_cpy(data));
+	new_elem = ft_create_elem(data);
 	if (*begin_list == NULL)
 	{
 		*begin_list = new_elem;
--- system/read_stdin/ft_read_stdin.c
+++ system/read_stdin/ft_read_stdin.c
@@ -10,8 +10,6 @@ t_list *get_stdin(int loc)
 		return (NULL);
 	begin_list = ft_create_elem(buf);
 	while (read(loc, buf, 1) > 0)
-	{
 		ft_list_push_back(&begin_list, buf);
-	}
 	return (begin_list);
 }
